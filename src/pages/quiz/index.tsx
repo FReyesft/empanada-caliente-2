7aafaee6c8b659d6cb4a95ace905ff3c751da7fe
FReyeSFT <f.reyesft@gmail.com>
2024-12-27 15:00:20 -0500

    (add)

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { useQuestions } from "../../hooks/useQuestions";
 import { useThemeSelected } from "../../store/theme-selected.store";
+import logo from '@assets/images/logo.avif';
+import './style.css';
+import { IonButton } from "@ionic/react";
+import { useHistory } from "react-router";
 
 const Quiz: React.FC = () => {
   const { themeSelected } = useThemeSelected();
   const { question, getRandomQuestion } = useQuestions();
   const [currentQuestion, setCurrentQuestion] = useState(question);
   const [previousQuestionId, setPreviousQuestionId] = useState<number | null>(null);
+  const history = useHistory();
+  
+  if(!themeSelected) {
+    history.push('/');  
+  }
 
   const handleNextQuestion = () => {
     if (currentQuestion?.id) {
@@ -17,10 +26,15 @@ const Quiz: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>{themeSelected?.name}</h2>
-      <p>{currentQuestion?.question}</p>
-      <button onClick={handleNextQuestion}>Siguiente</button>
+    <div className="container-all">
+      <h2 className="title">{themeSelected?.name}</h2>
+      <div className="container">
+        <img src={logo}></img>
+        <div className="question-container">
+          <p className="current-question">{currentQuestion.question}</p>
+          <IonButton onClick={handleNextQuestion}>Cambiar pregunta</IonButton>
+        </div>
+      </div>
     </div>
   );
 };
